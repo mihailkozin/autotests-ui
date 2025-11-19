126f44e3e4e170cba322375150c5e2733c8fbe01
mihailkozin <91541156+mihailkozin@users.noreply.github.com>
2025-11-19 10:57:12 +0500

    Введение в laywright. Практика с laywright

---
+++ playwright_authorization.py
@@ -0,0 +1,34 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    # Открываем браузер и создаем новую страницу
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    # Переходим на страницу входа
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    # Заполняем поле email
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
+
+    # Заполняем поле username
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill("username")
+
+    # Заполняем поле пароль
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill("password")
+
+    # Нажимаем на кнопку Registration
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    # На странице отображается заголовок "Dashboard"
+    dashbord_toolbar_tittle = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashbord_toolbar_tittle).to_be_visible()
+    expect(dashbord_toolbar_tittle).to_have_text("Dashboard")
+
+
+
+
