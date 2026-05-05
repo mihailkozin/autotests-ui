4f9c41b0d7e7eb78b17e1f6ae9860f28a47e714d
mihailkozin <km.kozin56@gmail.com>
2026-05-05 16:41:55 +0500

    "Управление автотестами. Фикстуры Pytest."

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,17 @@
 import pytest
-from playwright.sync_api import Playwright, Page, sync_playwright
-
+from playwright.sync_api import Page, sync_playwright
 
 
 @pytest.fixture
-def chromium_page(playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    yield browser.new_page()
-    browser.close()
+def chromium_page() -> Page:
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        yield page
+        context.close()
+        browser.close()
+
 
 @pytest.fixture(scope='session')
 def initialize_browser_state():
@@ -38,13 +42,14 @@ def initialize_browser_state():
 
 
 @pytest.fixture
-def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+def chromium_page_with_state(initialize_browser_state) -> Page:
     print("Создаю страницу с сохраненным состоянием...")
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json")
-    page = context.new_page()
-    yield page
-    context.close()
-    browser.close()
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state="browser-state.json")
+        page = context.new_page()
+        yield page
+        context.close()
+        browser.close()
 
 
