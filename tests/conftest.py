643c72ae110b1ba759b506ad4a259eb026727c61
mihailkozin <km.kozin56@gmail.com>
2026-05-05 10:57:44 +0500

    "Управление автотестами. Фикстуры Pytest."

---
+++ tests/conftest.py
@@ -0,0 +1,50 @@
+import pytest
+from playwright.sync_api import Playwright, Page, sync_playwright
+
+
+
+@pytest.fixture
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
+
+@pytest.fixture(scope='session')
+def initialize_browser_state():
+    print("Начинаю регистрацию пользователя...")
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+        # Заполняем форму рег.
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill("user.name@gmail.com")
+        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        username_input.fill("username")
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill("password")
+
+        # Нажимаем на кнопку Registration
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.click()
+
+        # Сохранение состояния сессии в файл, закрываем браузер
+        context.storage_state(path="browser-state.json")
+        print("Состояние сохранено в browser-state.json")
+        context.close()
+        browser.close()
+
+
+@pytest.fixture
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    print("Создаю страницу с сохраненным состоянием...")
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+    yield page
+    context.close()
+    browser.close()
+
+
